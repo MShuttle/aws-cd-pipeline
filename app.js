675ec59e377e9d70bb137e72f75ec5c557312845
mshuttle <markusrow@gmail.com>
2021-08-23 11:20:18 +1000

    Change Message for Full Pipeline TEST

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello CD Pipeline Tutorial!'));
+app.get('/', (req, res) => res.send('Hello CD Pipeline Tutorial! Full Pipeline TEST'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
